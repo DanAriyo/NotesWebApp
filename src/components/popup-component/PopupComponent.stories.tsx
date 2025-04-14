c69c40d680d2aabd83535de89c676c61a3279b21
DanAriyo <oluwatobi.ariyo@studio.unibo.it>
2025-04-14 10:48:47 +0200

    added to the popup component stories the children InputTextComponent and InputTextAreaComponent

--- src/components/popup-component/PopupComponent.stories.tsx
+++ src/components/popup-component/PopupComponent.stories.tsx
@@ -2,9 +2,14 @@ import { Story } from "@ladle/react";
 import { PopupComponent } from "./PopupComponent";
 import { useState } from "react";
 import { ButtonComponent } from "../button-component/ButtonComponent";
+import { InputTextAreaComponent } from "../input-textarea-component/InputTextAreaComponent";
+import { InputTextComponent } from "../input-text-component/InputTextComponent";
 
 export const Popup: Story = () => {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [value, valueChanged] = useState("");
+  const [title, setTitle] = useState("");
+
   return (
     <div className='box'>
       <ButtonComponent title='clickMe' onClick={() => setIsPopupOpen(true)} />
@@ -15,7 +20,20 @@ export const Popup: Story = () => {
         dialogPanelClassName='flex-col-bg-white-m-5-p-5-rounded-3xl'
         divClassName='custom-center-overlay'
       >
-        <textarea>La mia textarea di prova</textarea>
+        <InputTextComponent
+          value={title}
+          valueChanged={setTitle}
+          isEditable={true}
+          placeholder={true}
+          className='p-1.5 border-2 m-1 font-semibold'
+        ></InputTextComponent>
+        <InputTextAreaComponent
+          value={value}
+          isEditable={true}
+          valueChanged={valueChanged}
+          placeHolder={true}
+          className='padding'
+        />
       </PopupComponent>
     </div>
   );
