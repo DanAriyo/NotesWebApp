297664148fe6976767b147898edc8d21960a1a62
DanAriyo <oluwatobi.ariyo@studio.unibo.it>
2025-04-18 11:42:11 +0200

    improved the UI by removing the button and rendere the entire box clickable

--- src/pages/notes/components/NoteListItemComponent.tsx
+++ src/pages/notes/components/NoteListItemComponent.tsx
@@ -11,17 +11,13 @@ function NoteListItemComponent({ notes, noteClicked }: NoteListItemProps) {
     <>
       <div className='defaultNote'>
         {notes?.map((note) => (
-          <div key={note.id} className=' box'>
+          <div
+            key={note.id}
+            className=' box cursor-pointer hover:shadow-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition-all duration-200'
+            onClick={() => noteClicked?.(note)}
+          >
             <h2 className='text-center text-2xl m-1.5'>{note.title}</h2>
             <p className='text-center font-sans  m-1.5'>{note.description}</p>
-            <div className='flex flex-col items-center'>
-              <ButtonComponent
-                title='Visualizza'
-                onClick={() => noteClicked?.(note)}
-                variant='btn-secondary hidden-on-hover'
-                size='btn-md'
-              />
-            </div>
           </div>
         ))}
       </div>
