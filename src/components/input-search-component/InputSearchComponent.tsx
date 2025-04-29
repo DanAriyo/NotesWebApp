4c9da54d605cbc009440a234c050c201dd7cd3a1
DanAriyo <oluwatobi.ariyo@studio.unibo.it>
2025-04-29 08:59:03 +0200

    modified the size of the input search component

--- src/components/input-search-component/InputSearchComponent.tsx
+++ src/components/input-search-component/InputSearchComponent.tsx
@@ -14,8 +14,10 @@ function InputSearchComponent({
   icon,
 }: InputSearchProps) {
   return (
-    <div className='flex flex-col justify-center m-5 p-1 relative w-[70%] md:w-[50%] '>
-      <span className='absolute left-2 text-gray-400'>{icon}</span>
+    <div className='flex flex-col items-center m-5 p-1 relative w-[70%] md:w-[90%] '>
+      <span className='absolute left-9 bottom-4 md:left-98 text-gray-400'>
+        {icon}
+      </span>
       <input
         type='text'
         id='filter'
